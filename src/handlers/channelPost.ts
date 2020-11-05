dafd0cec4b298d0522f568f51b3fe8679a893836
Andrew Lane <contact@lungers.com>
2020-11-04 22:56:52 -0500

    Add /connect as an alias for connecting channels & update success message

--- src/handlers/channelPost.ts
+++ src/handlers/channelPost.ts
@@ -3,9 +3,9 @@ import { TBot } from '../typings';
 
 export default (bot: TBot): void => {
     bot.on('channel_post', async ctx => {
-        if (
-            ctx.channelPost!.text?.toLowerCase() === `@${ctx.me}`.toLowerCase()
-        ) {
+        const connectCommands: string[] = ['/connect', `@${bot.context.me}`.toLowerCase()];
+
+        if (ctx.channelPost!.text && connectCommands.includes(ctx.channelPost!.text.toLowerCase())) {
             const { title, username } = ctx.chat!;
             const admins = (await ctx.getChatAdministrators())
                 .map(({ user: { id } }) => id)
@@ -17,9 +17,7 @@ export default (bot: TBot): void => {
                 { upsert: true },
             );
 
-            const reply = await ctx.reply(
-                'Success. This message will automatically be deleted in 5 seconds.',
-            );
+            const reply = await ctx.reply('Your channel has been connected 👍');
             ctx.deleteMessage();
 
             setTimeout(() => {
