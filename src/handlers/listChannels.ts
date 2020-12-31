f9c4b20b730d1d1778b98c32e6901b19e137e24f
Andrew Lane <contact@lungers.com>
2020-12-31 17:16:25 -0500

    Specify when someone doesn't have any channels in /channels

--- src/handlers/listChannels.ts
+++ src/handlers/listChannels.ts
@@ -4,8 +4,12 @@ import { InlineKeyboardMarkup } from 'telegraf/typings/telegram-types';
 import { TBot } from '../typings';
 
 export default (bot: TBot): void => {
-    const getChannelButtons = async (userId: number): Promise<InlineKeyboardMarkup['inline_keyboard']> => {
+    const getChannelButtons = async (userId: number): Promise<InlineKeyboardMarkup['inline_keyboard'] | null> => {
         const channels = await Channel.find({ admins: { $in: [userId] } });
+
+        if (channels.length === 0) {
+            return null;
+        }
 
         return channels.map(channel => {
             return [
@@ -26,7 +30,12 @@ export default (bot: TBot): void => {
         Composer.privateChat(async ctx => {
             const buttons = await getChannelButtons(ctx.from!.id);
 
-            ctx.reply('Here\'s a list of channels that I\'m admin in with you.', {
+            if (buttons === null) {
+                await ctx.reply('You don\'t have any channels.');
+                return;
+            }
+
+            await ctx.reply('Here\'s a list of channels that I\'m admin in with you.', {
                 reply_markup: {
                     inline_keyboard: buttons,
                 },
@@ -53,7 +62,12 @@ export default (bot: TBot): void => {
 
         if (!admins.includes(ctx.from!.id)) {
             const buttons = await getChannelButtons(ctx.from!.id);
-            await ctx.editMessageReplyMarkup({ inline_keyboard: buttons });
+
+            if (buttons === null) {
+                await ctx.editMessageText('You don\'t have any channels.');
+            } else {
+                await ctx.editMessageReplyMarkup({ inline_keyboard: buttons });
+            }
         }
 
         await ctx.answerCbQuery('Updated 👍');
